8bfec98d287b28d4f237b24b935e13042802d55f
Holly <holly_cooper@live.com.au>
2020-11-11 21:44:11 +1000

    sql

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+#3 /// for relative path, 4 //// for absolute path - will stick with relative for now.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(400), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+def __repr__(self):
+    return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
